a74c462904056b6d38d227d26676b54645ec388c
Ezequiel <ezequielcansler91@gmail.com>
2025-02-11 14:35:17 -0300

    Agregar cambios en Entidades y otros archivos

---
+++ Entidades/Modelos/Cliente.vb
@@ -0,0 +1,16 @@
+﻿Public Class Cliente
+    Public Property ClienteID As Integer
+    Public Property Nombre As String
+    Public Property Apellido As String
+    Public Property Email As String
+    Public Property Telefono As String
+
+    Public Sub New(clienteID As Integer, nombre As String, apellido As String, email As String, telefono As String)
+        Me.ClienteID = clienteID
+        Me.Nombre = nombre
+        Me.Apellido = apellido
+        Me.Email = email
+        Me.Telefono = telefono
+    End Sub
+
+End Class
---
+++ Entidades/Modelos/Venta.vb
@@ -0,0 +1,16 @@
+﻿Public Class Venta
+    Public Property VentaID As Integer
+    Public Property ClienteID As Integer
+    Public Property Fecha As DateTime
+    Public Property MontoTotal As Decimal
+    Public Property ProductoID As Integer
+
+    Public Sub New(ventaID As Integer, clienteID As Integer, fecha As DateTime, montoTotal As Decimal, productoID As Integer)
+        Me.VentaID = ventaID
+        Me.ClienteID = clienteID
+        Me.Fecha = fecha
+        Me.MontoTotal = montoTotal
+        Me.ProductoID = productoID
+    End Sub
+
+End Class
